1c056b6f0fffaf52e7e2fc3423b429dcc179da01
suhaib <suhaibvbm@gmail.com>
2023-11-25 14:27:44 +0530

    Adding to exel

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import requests
+from datetime import datetime
 
 END_POINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
 APP_ID = "APP_ID"
 APP_KEY = "APP_KEY"
 
+SHEET_ENDPOINT = "your sheet enpoint"
 
 exercise_text = input("Tell which exercise you did: ")
 
@@ -24,3 +26,21 @@ parameter = {
 response = requests.post(END_POINT, json=parameter, headers=headers)
 result = response.json()
 print(result)
+
+#create account on https://dashboard.sheety.com and get post add enabled
+today_date = datetime.now().strftime("%x")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_input ={
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    sheet_response = requests.post(SHEET_ENDPOINT,json=sheet_input)
+    print(sheet_response.text)
